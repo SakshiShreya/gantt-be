7c242082b1f5901a4e5338f0c251cdaae140661a
SakshiShreya <shreyasakshi96@gmail.com>
2022-08-20 18:45:17 +0530

    Setup typescript

--- server.js
+++
@@ -1,7 +0,0 @@
-import app from "./app.js";
-
-console.log("Hi")
-
-app.listen(8000, () => {
-  console.log("App listening on port 8000");
-})
---
+++ server.ts
@@ -0,0 +1,7 @@
+import app from "./app";
+
+const PORT = 8000;
+
+app.listen(8000, () => {
+  console.log("App listening on port" + PORT);
+})
